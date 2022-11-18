583d5a67c93ef91e25cfa16fed2dc5a7bce285a8
Mohanjeet Singh <110851441+mohanjeetsingh@users.noreply.github.com>
2022-11-19 01:01:41 +0530

    M stars io (#1)
    * Branch test
    
    * First version mStars Lazy
    
    * First version Lazy
    
    * Updated to Firebase 9.14.0

---
+++ loadscripts.js
@@ -0,0 +1,2 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-database.js";
